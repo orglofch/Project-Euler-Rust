44c12334115b3dfb26cce2324f576766186bbcb4
Owen Glofcheski <owen.glofcheski@gmail.com>
2017-08-01 23:43:55 -0700

    Finished problem27

--- problem27.rs
+++ problem27.rs
@@ -1,4 +1,4 @@
-// TODO
+// DONE: -59231
 //
 // Fairly simply, especially if we already have a hard coded
 // list of primes, but a cache with a calculator should suffice.
@@ -20,24 +20,24 @@
 // n also has to include 0. Which implies that b itself
 // need to be a positive prime number.
 //
+// 1 + a + b (n = 1) needs to be a prime and since b is
+// necessarily odd (prime) a must be odd, otherwise
+// a + 1 is odd and two odds form an even.
+//
 // If we really wanted to go overboard we could perform
 // random hillclimbing (since I'm sure there are multiple
 // local maxima) and use that to try to approximate it.
 // Perhaps the function isn't smooth enough though for
 // that to be an effective strategy.
+//
+// For a slight speedup we can also jump right to
+// n = our_longest_sequence + 1 to early exit on some
+// small subsequences.
 
 use std::collections::HashSet;
 
-fn is_prime(num: i32, cache: &HashSet<u32>) -> bool {
-    if num < 0 {
-        return false
-    }
-
-    if cache.contains(&(num as u32)) {
-        return true;
-    }
-
-    for prime in cache.iter() {
+fn is_prime(num: i32, lesser_primes: &Vec<u32>) -> bool {
+    for prime in lesser_primes.iter() {
         if (num as u32) % prime == 0 {
             return false;
         }
@@ -45,39 +45,45 @@ fn is_prime(num: i32, cache: &HashSet<u32>) -> bool {
     return true;
 }
 
-fn make_primes() -> HashSet<u32> {
+fn make_primes() -> (HashSet<u32>, Vec<u32>) {
     let mut primes: HashSet<u32> = HashSet::new();
+    let mut lesser_primes: Vec<u32> = Vec::new();
 
     for i in 2..86241 {
-        if is_prime(i as i32, &primes) {
+        if is_prime(i as i32, &lesser_primes) {
+            lesser_primes.push(i);
             primes.insert(i);
         }
     }
-    return primes;
+    return (primes, lesser_primes);
 }
 
+#[inline]
 fn quadratic(n: u32, a: i32, b: u32) -> i32 {
     let i32n: i32 = n as i32;
-    return i32n*i32n + a * i32n + b as i32;
+    return i32n * i32n + a * i32n + b as i32;
 }
 
 fn quadratic_prime_product() -> i32 {
-    let primes = make_primes();
+    let (prime_cache, prime_list) = make_primes();
 
     let mut max_run: u32 = 0;
     let mut max_a: i32 = 0;
     let mut max_b: u32 = 0;
 
-    for a in -1000..1001 {
-        // TODO(orglofch): limit b to primes.
-        for b in primes.iter() {
-            if *b > 1000 {
-                break;
+    for a in (-999..1000).filter(|&n| n % 2 != 0) {
+        for b in prime_list.iter().take_while(|&&n| n < 1000) {
+            // Skip sequences that are non-zero but smaller than our
+            // max so far.
+            let mut quad = quadratic(max_run + 1, a, *b);
+            if quad < 0 || !prime_cache.contains(&(quad as u32)) {
+                continue;
             }
 
+            // Start from the beginning again to verify it's a full sequence.
             let mut num: u32 = 0;
-            let mut quad = quadratic(num, a, *b);
-            while is_prime(quad, &primes) {
+            quad = quadratic(num, a, *b);
+            while quad > 0 && prime_cache.contains(&(quad as u32)) {
                 num += 1;
                 quad = quadratic(num, a, *b);
             }
@@ -86,12 +92,12 @@ fn quadratic_prime_product() -> i32 {
                 max_a = a;
                 max_b = *b;
             }
-            println!("n^2 + {} * n + {} has run {}", a, b, num);
         }
     }
     return max_a * max_b as i32;
 }
 
 fn main() {
-    println!("The quadratic prime product is {}", quadratic_prime_product());
+    println!("The quadratic prime product is {}",
+             quadratic_prime_product());
 }
