58af98547aa88ef15b8f6d2a293a01d965751531
Owen Glofcheski <owen.glofcheski@gmail.com>
2018-04-30 10:12:49 -0700

    Finished problem 53

--- src/p053.rs
+++ src/p053.rs
@@ -1,13 +1,44 @@
-// TODO
+// DONE: 4075
 //
-// Choose is symetric about it's center and is usually (always?)
-// greatest around the center so we can cut the work in half.
+// We can take advantage of 2 simple identities to cut the work down significantly.
+// 1) nCk = (n - 1)Ck + (n - 1)C(k - 1) - The dynamic programming recurrence.
+// 2) nCk = nC(n - k) - The combination are symmetric, cutting the work in half.
 //
+// We can iterate from the center upwards and stop once we've
+// hit a value < 1,000,000 since
+// n!/((k - 1)!(n - k + 1)!) = (k * n!) / (k!(n - k)!(n - k + 1))
+//                           = k/(n - k + 1) * nCk
+// Which is <= nCk if k/(n - k + 1) <= 1
 //
-// We can start around the center and work from there outwards
-// to decide whether we can find a value > 1_000_000.
-// This works because (n - r)!r! <= (n - r + 1)!(r - 1)! // TODO(orglofch): Is this true?
+// k <= (n - k + 1)
+// 2k <= n + 1, which is always true for k <= n / 2
 //
-// We also know that (n + 1)! / ((n + 1 - r)!r!) = (n + 1) * n! / ((n + 1 - r) * (n - r)!r!)
-// when transistioning between values of n. So we could technically just use a table
-// and work upwards. Or potentially use a table and work downwards.
+// Note, however, we can't take advantage of both so we'll just use the recurrence
+// relation and go from there if need be.
+
+fn combinations_greater_than_1_million() -> u32 {
+    // Note, truncation is fine since we don't are about higher order bits.
+    let mut cache = [[0u32; 101]; 101];
+
+    let mut count = 0;
+    for n in 1..101 {
+        cache[n][0] = 1;
+        cache[n][n] = 1;
+        cache[n][1] = n as u32;
+        cache[n][n - 1] = n as u32;
+        for k in 2..((n as f32 / 2.0).floor() as usize + 1) {
+            let comb = cache[n - 1][k] + cache[n - 1][k - 1];
+            cache[n][k] = comb;
+            cache[n][n - k] = comb;
+
+            if comb > 1_000_000 {
+                count += if n - k == k { 1 } else { 2 };
+            }
+        }
+    }
+    return count;
+}
+
+fn main() {
+    println!("There are {} combinations > 1,000,000", combinations_greater_than_1_million());
+}
